e9c18d6b1626b726297557498e7be5031e3417c5
Pradhuman Yadav <71937736+pradhuman17@users.noreply.github.com>
2023-01-23 13:50:54 +0530

    Update rhombus.java

--- rhombus.java
+++ rhombus.java
@@ -22,6 +22,53 @@ public class pattern{
 
             }
             System.out.println();
+            
+            
+            
+            
+            
+            /*import java.util.Scanner;
+import java.lang.Math;
+public class pattern{
+    // rhombus pattern
+    public static void main(String[] Args)
+    {
+
+        int n =5;
+        for(int i =1; i<=n;i++)
+        {
+            for (int j = i; j <= n; j++)
+            {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <=i; j++)
+            {
+
+                if( i==1 || j==1 || i==n || j==n )
+                {
+                    System.out.print(" * ");
+                }
+                else
+                {
+                    System.out.print("   ");
+                }
+            }
+
+            for (int j = i; j <= n; j++)
+            {
+                if( i==1 || j==1 || i==n ||j==n)
+                {
+                    System.out.print(" * ");
+                }
+                else
+                {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}*/
         }
     }
 
