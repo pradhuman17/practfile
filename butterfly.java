a440db1c26a727013b30b59b98888924e8011c82
Pradhuman Yadav <71937736+pradhuman17@users.noreply.github.com>
2023-01-23 12:32:23 +0530

    Create butterfly.java

---
+++ butterfly.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class pattern{
+    //Butterfly pattern
+    public static void main(String[] Args)
+    {
+
+        int n =5;
+        for(int i =1; i<=n;i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+
+            for (int j = i; j < n; j++) {
+                System.out.print("   ");
+            }
+
+            for (int j = i; j < n; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+
+        }
+            for(int i =1; i<=n;i++)
+            {
+                for (int j = i; j <=n; j++)
+                {
+                    System.out.print(" * ");
+                }
+
+                for (int j = 1; j < i; j++) {
+                    System.out.print("   ");
+                }
+
+                for (int j = 1; j <i; j++)
+                {
+                    System.out.print("   ");
+                }
+                for (int j = i; j <= n; j++)
+                {
+                    System.out.print(" * ");
+                }
+                System.out.println();
+        }
+    }
+
+}
